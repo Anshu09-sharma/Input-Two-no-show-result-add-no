aeda8603dc7cf96a41b2c99401cd774ac0a7f2c2
Anshu09-sharma <anshubhardwaj9876@gmail.com>
2024-10-05 23:07:16 +0530

    Add files via upload

---
+++ Add two no.c
@@ -0,0 +1,11 @@
+//Input Two no & show result after adding two no
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("Enter the value of x & y");
+    scanf("%d%d",&x,&y);
+    z=x+y;
+    printf("\nSum is %d",z);
+    return 0;
+}
